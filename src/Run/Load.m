a48491cbe6b9c9e8fd154a4ed33476031ecf9d60
FabioRagazzi <fabio.ragazzi.98@gmail.com>
2025-11-11 11:11:28 +0000

    Fixed problem in Load

--- src/Run/Load.m
+++ src/Run/Load.m
@@ -2,16 +2,7 @@ function [out_pp] = Load(name)
 
 out_pp = load(name);
 
-% for back compatibility (can be removed when distributing code) ----------
-if ~isfield(out_pp.input.p,"MSHparameters")                           %   |
-    input = out_pp.input;                                             %   |                       
-    input.p.MSHparameters = struct;                                   %   |
-    out_pp.input = input;                                             %   |
-    save(name,"input","-append")                                      %   |
-end                                                                   %   |
-% -------------------------------------------------------------------------
-
-out_pp.msh = GetMesh(out_pp.input.geo_file_content, out_pp.input.p.COORDINATES, out_pp.input.p.MSHparameters);
+out_pp.msh = GetMesh(out_pp.input.geo_file_content, out_pp.input.p.COORDINATES, out_pp.input.p.MSH_PARAMETERS);
 
 out_pp = rmfield(out_pp,"y_end");
 
