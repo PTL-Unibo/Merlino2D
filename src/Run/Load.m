d033e6310d7293ba7c643584b01317e823b67c95
FabioRagazzi <fabio.ragazzi.98@gmail.com>
2026-01-03 20:04:55 +0100

    Removed COORDONATES from Load

--- src/Run/Load.m
+++ src/Run/Load.m
@@ -2,7 +2,7 @@ function [out_pp] = Load(name)
 
 out_pp = load(name);
 
-out_pp.msh = GetMesh(out_pp.input.geo_file_content, out_pp.input.p.COORDINATES, out_pp.input.p.MSH_PARAMETERS);
+out_pp.msh = GetMesh(out_pp.input.geo_file_content, "cartesian", out_pp.input.p.MSH_PARAMETERS);
 
 out_pp = rmfield(out_pp,"y_end");
 
