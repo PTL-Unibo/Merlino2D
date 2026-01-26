2599a8398862b0a289b8e7d22c27ede0f57c9551
FabioRagazzi <fabio.ragazzi.98@gmail.com>
2026-01-26 12:09:17 +0100

    Fixed time digits problem in PVD files

--- src/Vtu/GeneratePVD.m
+++ src/Vtu/GeneratePVD.m
@@ -6,7 +6,7 @@ fprintf(fileID, "<VTKFile type=""Collection"" version=""0.1"" byte_order=""Littl
 fprintf(fileID, "  <Collection>\n");
 for k = 1:numel(tout)
     k_str = num2str(k);
-    fprintf(fileID, "    <DataSet timestep=""" + num2str(tout(k)) + """ file=""" + name + repmat('0',1,num_digits-strlength(k_str)) + k_str + ".vtu""/>\n");
+    fprintf(fileID, "    <DataSet timestep=""" + num2str(tout(k),10) + """ file=""" + name + repmat('0',1,num_digits-strlength(k_str)) + k_str + ".vtu""/>\n");
 end
 fprintf(fileID, "  </Collection>\n");
 fprintf(fileID, "</VTKFile>\n");
