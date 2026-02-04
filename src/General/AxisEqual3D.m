e289833e0884ee3c761b79a2e62d138a8f9d7dde
FabioRagazzi <fabio.ragazzi.98@gmail.com>
2026-02-04 17:59:38 +0100

    Created MeshViewer

--- src/General/AxisEqual3D.m
+++ src/General/AxisEqual3D.m
@@ -1,5 +1,7 @@
-function [] = AxisEqual3D()
-    ax = gca();
+function [] = AxisEqual3D(ax)
+    if nargin < 1
+        ax = gca();
+    end
     ax.PlotBoxAspectRatio = [(ax.XLim(2)-ax.XLim(1))/(ax.YLim(2)-ax.YLim(1)), 1, 1];
 end
 
